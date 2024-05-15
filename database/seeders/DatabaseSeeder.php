3ce8f90abc220f4e67f196f58a741708949702dc
EL-Amrani Youssef <elamraniy292@gmail.com>
2024-05-15 20:03:27 +0100

    added mock data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,12 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('users')->insert([
-            ['name' => 'khalid', 'email' => 'ledbenh.dofy@proton.me', 'email_verified_at' => '2024-04-23 09:27:33', 'password' => Hash::make('12345678'), 'remember_token' => 'TXoLy1Fayi', 'phone' => '0648446216', 'pin' => '123456', 'type' => '2', 'updated_at' => '2024-04-23 09:27:35', 'created_at' => '2024-04-23 09:27:35'],
-            ['name' => 'khalid', 'email' => 'hello@hello', 'email_verified_at' => '2024-04-23 09:27:33', 'password' => Hash::make('12345678'), 'remember_token' => 'TXoLy1Fayi', 'phone' => '0648446216', 'pin' => '123455', 'type' => '2', 'updated_at' => '2024-04-23 09:27:35', 'created_at' => '2024-04-23 09:27:35'],
-        ]
-        );
-
-
-
-
-
-
-    }
-    }
+        // Mock data
+        DB::table('users')->insert(
+            [
+                ['name' => 'Manager', 'email' => 'ledbenh.dofy@proton.me', 'email_verified_at' => '2024-04-23 09:27:33', 'password' => Hash::make('12345678'), 'remember_token' => 'TXoLy1Fayi', 'phone' => '0648446216', 'pin' => '123456', 'type' => 2, 'updated_at' => '2024-04-23 09:27:35', 'created_at' => '2024-04-23 09:27:35'],
+                ['name' => 'admin', 'email' => 'hello@hello', 'email_verified_at' => '2024-04-23 09:27:33', 'password' => Hash::make('12345678'), 'remember_token' => 'TXoLy1Fayi', 'phone' => '0648446216', 'pin' => '123455', 'type' => 1, 'updated_at' => '2024-04-23 09:27:35', 'created_at' => '2024-04-23 09:27:35'],
+                ['name' => 'user', 'email' => 'hello2@hello', 'email_verified_at' => '2024-04-23 09:27:33', 'password' => Hash::make('12345678'), 'remember_token' => 'TXoLy1Fayi', 'phone' => '0648446216', 'pin' => '123454', 'type' => 0, 'updated_at' => '2024-04-23 09:27:35', 'created_at' => '2024-04-23 09:27:35'],
+            ]
+        );}
+}
